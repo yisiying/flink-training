827c370bda971a3c1c655c9a9ec2e7f3ff03fd3c
yisiying <2574989595@qq.com>
2021-04-25 19:38:21 +0800

    ride cleansing

--- ride-cleansing/src/main/java/org/apache/flink/training/exercises/ridecleansing/RideCleansingExercise.java
+++ ride-cleansing/src/main/java/org/apache/flink/training/exercises/ridecleansing/RideCleansingExercise.java
@@ -24,6 +24,7 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.training.exercises.common.datatypes.TaxiRide;
 import org.apache.flink.training.exercises.common.sources.TaxiRideGenerator;
 import org.apache.flink.training.exercises.common.utils.ExerciseBase;
+import org.apache.flink.training.exercises.common.utils.GeoUtils;
 import org.apache.flink.training.exercises.common.utils.MissingSolutionException;
 
 /**
@@ -31,41 +32,40 @@ import org.apache.flink.training.exercises.common.utils.MissingSolutionException
  *
  * <p>The task of the exercise is to filter a data stream of taxi ride records to keep only rides that
  * start and end within New York City. The resulting stream should be printed.
- *
  */
 public class RideCleansingExercise extends ExerciseBase {
 
-	/**
-	 * Main method.
-	 *
-	 * @throws Exception which occurs during job execution.
-	 */
-	public static void main(String[] args) throws Exception {
+    /**
+     * Main method.
+     *
+     * @throws Exception which occurs during job execution.
+     */
+    public static void main(String[] args) throws Exception {
 
-		// set up streaming execution environment
-		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-		env.setParallelism(ExerciseBase.parallelism);
+        // set up streaming execution environment
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(ExerciseBase.parallelism);
 
-		// start the data generator
-		DataStream<TaxiRide> rides = env.addSource(rideSourceOrTest(new TaxiRideGenerator()));
+        // start the data generator
+        DataStream<TaxiRide> rides = env.addSource(rideSourceOrTest(new TaxiRideGenerator()));
 
-		DataStream<TaxiRide> filteredRides = rides
-				// filter out rides that do not start or stop in NYC
-				.filter(new NYCFilter());
+        DataStream<TaxiRide> filteredRides = rides
+                // filter out rides that do not start or stop in NYC
+                .filter(new NYCFilter());
 
-		// print the filtered stream
-		printOrTest(filteredRides);
+        // print the filtered stream
+        printOrTest(filteredRides);
 
-		// run the cleansing pipeline
-		env.execute("Taxi Ride Cleansing");
-	}
+        // run the cleansing pipeline
+        env.execute("Taxi Ride Cleansing");
+    }
 
-	private static class NYCFilter implements FilterFunction<TaxiRide> {
+    private static class NYCFilter implements FilterFunction<TaxiRide> {
 
-		@Override
-		public boolean filter(TaxiRide taxiRide) throws Exception {
-			throw new MissingSolutionException();
-		}
-	}
+        @Override
+        public boolean filter(TaxiRide taxiRide) throws Exception {
+            return GeoUtils.isInNYC(taxiRide.endLon, taxiRide.endLat) || GeoUtils.isInNYC(taxiRide.startLon, taxiRide.startLat);
+        }
+    }
 
 }
